8f602163f85a45050e729c4f6536dae7c4f3949c
chaerlo127 <monynony0203@gmail.com>
2022-04-09 18:25:02 +0900

    arraylist concept

---
+++ src/list/arraylist/api/Main.java
@@ -0,0 +1,40 @@
+package list.arraylist.api;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(40);
+		System.out.println("add: " + numbers);
+		
+		numbers.add(2, 50);
+		System.out.println("add: " + numbers);
+		
+		numbers.remove(2);
+		System.out.println("remove: " + numbers);
+		
+		
+		System.out.println("get index 2: " + numbers.get(2));
+		
+		System.out.println("arraylist size: " + numbers.size());
+		
+		System.out.println("indexOf about 30:"  + numbers.indexOf(30));
+		
+		Iterator it = numbers.iterator();
+		while(it.hasNext()) {
+			int value = (int) it.next();
+			if(value == 30) {
+				it.remove();
+			}
+			System.out.println(value);
+		}
+		System.out.println(numbers);
+	}
+
+}
